cadfbfa6d0b223e43236546078b9d34e201681e1
Corey Gibson <cgibsonmmdev@gmail.com>
2020-08-13 08:06:02 -0400

    Updated scratch

--- scratch.js
+++ scratch.js
@@ -1,4 +1,4 @@
-class Dino {
+class Bees {
   constructor(name) {
       this.name = name;
       this.next = null
@@ -6,15 +6,4 @@ class Dino {
 }
 
 
-let anthony = new Dino("anthony");
-anthony.next = new Dino("daeqwon");
 
-let daeqwon = anthony.next;
-daeqwon.next = new Dino("sabrina");
-
-let sabrina = daeqwon.next;
-sabrina.next = new Dino("Angie");
-
-console.log(anthony);
-console.log(anthony.next.next.next);
-console.log(typeof anthony.next.next.next);
